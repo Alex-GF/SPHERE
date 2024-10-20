82c7955a99c90e51695658fb49a153b28857aec4
Alex-GF <yalejandro9@gmail.com>
2024-10-20 13:01:23 +0200

    FEAT: ADDED YAML CODE EDITION TO EDITOR

--- src/pages/pricing2yaml-editor/index.tsx
+++ src/pages/pricing2yaml-editor/index.tsx
@@ -1,17 +1,35 @@
 import Grid from "@mui/material/Grid2";
+import Editor from "@monaco-editor/react";
 
 export default function EditorPage() {
+  function handleEditorChange(value: string | undefined) {
+    if (value) {
+    }
+  }
+
   return (
-    <Grid container spacing={2} sx={{
-      width: '100%',
-      height: '100%'
-    }}>
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <Grid size={6}>
-        Test left
+        <Editor
+          height="100%"
+          defaultLanguage="yaml"
+          onChange={handleEditorChange}
+          theme="vs-dark"
+          options={{
+            minimap: {
+              enabled: false
+            }
+          }}
+        />
       </Grid>
-      <Grid size={6}>
-        Test right
-      </Grid>
+      <Grid size={6}>Test right</Grid>
     </Grid>
   );
 }
