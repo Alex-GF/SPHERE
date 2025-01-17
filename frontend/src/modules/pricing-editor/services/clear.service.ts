73ef9492fa63388edf1aef915591d7208b5532d4
Alex-GF <yalejandro9@gmail.com>
2025-01-17 19:28:13 +0100

    fix: identified bugs

--- frontend/src/modules/pricing-editor/services/clear.service.ts
+++ frontend/src/modules/pricing-editor/services/clear.service.ts
@@ -3,19 +3,20 @@ export function getClearEditorValue(){
     const currentDate = new Date().toISOString().split('T')[0];
 
     return `saasName: Your SaaS Name
-version: "2.0"
-createdAt: ${currentDate}
+syntaxVersion: "2.1"
+version: "latest"
+createdAt: 2024-01-15
 currency: EUR
 hasAnnualPayment: false
 features:
-feature1:
+  feature1:
     description: Feature 1 description
     valueType: BOOLEAN
     defaultValue: true
     type: DOMAIN
 usageLimits: null
 plans:
-BASIC:
+  BASIC:
     description: Basic plan
     price: 0.0
     unit: user/month
