96243c465f466738c04d495a6b10c571e641e634
Alex-GF <yalejandro9@gmail.com>
2025-02-16 20:31:17 +0100

    feat: updated landing pictures

--- frontend/src/modules/presentation/components/styled-button-landing/index.tsx
+++ frontend/src/modules/presentation/components/styled-button-landing/index.tsx
@@ -1,7 +1,9 @@
 import { styled, Button } from '@mui/material';
+import { primary } from '../../../core/theme/palette';
 
 export const StyledButtonLanding = styled(Button)({
-  '&:hover': {
-    color: 'white',
-  },
+  textAlign: 'center',
+  backgroundColor: primary[300],
+  transition: 'background-color color .5s',
+  '&:hover': { backgroundColor: primary[500], color: 'white' },
 });
